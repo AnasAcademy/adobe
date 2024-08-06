23e3e26115607ca880e01adf3d1c5121e38be125
emannasr-0 <eman.n@anasacademy.uk>
2024-08-06 15:51:58 +0300

    second commit

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\App;
+
 use App\Models\Appointment;
 use App\Models\illustrator_appointment;
 use App\Models\illustrator_email;
@@ -153,6 +155,7 @@ class AppointmentController extends Controller
     }
     public function validation(Request $request)
     {
+        App::setLocale('ar');
         $validator = Validator::make($request->all(), [
             'ar_name' => 'required|string|regex:/^[\p{Arabic} ]+$/u|max:255|min:5',
             'en_name' => 'required|string|regex:/^[a-zA-Z\s]+$/|max:255|min:5',
@@ -162,11 +165,11 @@ class AppointmentController extends Controller
                 function ($attribute, $value, $fail) use ($request) {
                     if ($request->input('action') === 'new') {
                         if (is_null($value)) {
-                            $fail('The test type field is required when action is new.');
+                            $fail(trans('appointment.The test type field is required when action is new.'));
                         } else {
                             $allowedValues = ['illustrator', 'photoshop', 'design', 'photoshop_design', 'photoshop_illustrator'];
                             if (!in_array($value, $allowedValues)) {
-                                $fail('The selected test type is invalid.');
+                                $fail(trans('appointment.The selected test type is invalid.'));
                             }
                         }
                     }
@@ -181,7 +184,7 @@ class AppointmentController extends Controller
                     $action = $request->action;
                     $email = Appointment::where('email', $value)->first();
                     if ($action == 'new' && $email) {
-                        $fail('The email already has an appointment.');
+                        $fail(trans('appointment.The email already has an appointment.'));
                     }
                 },
             ],
@@ -195,9 +198,9 @@ class AppointmentController extends Controller
                     $action = $request->action;
                     $appointment = photoshop_appointment::where('appointment_date', $value)->first();
                     if ($action == 'new' && ($test_type == 'photoshop' || $test_type == 'photoshop_design' || $test_type == 'photoshop_illustrator') && !$appointment) {
-                        $fail('The selected photoshop appointment date is invalid.');
+                        $fail(trans('appointment.The selected photoshop appointment date is invalid.'));
                     } elseif ($action == 'new' && ($test_type == 'photoshop' || $test_type == 'photoshop_design' || $test_type == 'photoshop_illustrator') && $appointment && $appointment->user_count == 0) {
-                        $fail('The selected Photoshop appointment date is fully booked.');
+                        $fail(trans('appointment.The selected Photoshop appointment date is fully booked.'));
                     }
                 },
             ],
@@ -207,9 +210,9 @@ class AppointmentController extends Controller
                     $action = $request->action;
                     $appointment = illustrator_appointment::where('appointment_date', $value)->first();
                     if ($action == 'new' && ($test_type == 'illustrator' || $test_type == 'photoshop_illustrator') && !$appointment) {
-                        $fail('The selected Illustrator appointment date is invalid.');
+                        $fail(trans('appointment.The selected Illustrator appointment date is invalid.'));
                     } elseif ($action == 'new' && ($test_type == 'illustrator' || $test_type == 'photoshop_illustrator') && $appointment && $appointment->user_count == 0) {
-                        $fail('The selected Illustrator appointment date is fully booked.');
+                        $fail(trans('appointment.The selected Illustrator appointment date is fully booked.'));
                     }
                 },
             ],
@@ -219,9 +222,9 @@ class AppointmentController extends Controller
                     $action = $request->action;
                     $appointment = design_appointment::where('appointment_date', $value)->first();
                     if ($action == 'new' && ($test_type == 'design' || $test_type == 'photoshop_design') && !$appointment) {
-                        $fail('The selected design appointment date is invalid.');
+                        $fail(trans('appointment.The selected design appointment date is invalid.'));
                     } elseif ($action == 'new' && ($test_type == 'design' || $test_type == 'photoshop_design') && $appointment && $appointment->user_count == 0) {
-                        $fail('The selected Design appointment date is fully booked.');
+                        $fail(trans('appointment.The selected Design appointment date is fully booked.'));
                     }
                 },
             ],
@@ -231,9 +234,9 @@ class AppointmentController extends Controller
                     $action = $request->action;
                     $appointment = duplicated_appointment::where('appointment_date', $value)->first();
                     if ($action == 'duplicated' && !$appointment) {
-                        $fail('The selected duplicated appointment date is invalid.');
+                        $fail(trans('appointment.The selected duplicated appointment date is invalid.'));
                     } elseif ($action == 'duplicated' && $appointment && $appointment->user_count == 0) {
-                        $fail('The selected Design appointment date is fully booked.');
+                        $fail(trans('appointment.The selected Design appointment date is fully booked.'));
                     }
                 },
             ],
@@ -242,38 +245,8 @@ class AppointmentController extends Controller
             'Endorsement3' => 'accepted',
             'Endorsement4' => 'accepted',
             'Endorsement5' => 'accepted',
-        ], [
-            'ar_name.required' => 'The Arabic name field is required.',
-            'ar_name.regex' => 'The Arabic name must contain only Arabic characters and spaces.',
-            'ar_name.min' => 'The Arabic name must be at least 5 characters long.',
-            'en_name.required' => 'The English name field is required.',
-            'en_name.regex' => 'The English name must contain only letters and spaces.',
-            'en_name.min' => 'The English name must be at least 5 characters long.',
-            'email.required' => 'The email field is required.',
-            'email.email' => 'Please enter a valid email address.',
-            'email.regex' => 'The email format is invalid.',
-            'phone.required' => 'The phone field is required.',
-            'phone.numeric' => 'The phone number must be a valid number.',
-            'country.required' => 'The country field is required.',
-            'country.min' => 'The country name must be at least 3 characters long.',
-            'city.required' => 'The city field is required.',
-            'city.min' => 'The city name must be at least 3 characters long.',
-            'diploma.required' => 'The diploma field is required.',
-            'diploma.min' => 'The diploma name must be at least 3 characters long.',
-            'action.required' => 'The action field is required.',
-            'action.in' => 'The selected action is invalid.',
-            'test_type.required' => 'The test type field is required.',
-            'test_type.in' => 'The selected test type is invalid.',
-            'photoshop_appointment_date.required_if' => 'The Photoshop appointment date field is required when action is new and the test type is Photoshop.',
-            'illustrator_appointment_date.required_if' => 'The Illustrator appointment date field is required when test type is Illustrator.',
-            'design_appointment_date.required_if' => 'The design appointment date field is required when test type is Design.',
-            'duplicated_appointment_date.required_if' => 'The duplicated appointment date field is required when action is duplicated.',
-            // 'Endorsement1.accepted' => 'You must accept the first endorsement.',
-            'Endorsement2.accepted' => 'You must accept the second endorsement.',
-            'Endorsement3.accepted' => 'You must accept the third endorsement.',
-            'Endorsement4.accepted' => 'You must accept the forth endorsement.',
-            'Endorsement5.accepted' => 'You must accept the fifth endorsement.',
-        ]);
+        ], 
+      );
 
         if ($validator->fails()) {
             return response()->json(['errors' => $validator->errors()], 422);
